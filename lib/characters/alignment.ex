125efafdbb654abffa3a41de633bfb37bc61fc6d
Nessa Marin <nessa.murmur@gmail.com>
2016-08-10 10:45:44 -0700

    import -> alias

--- lib/characters/alignment.ex
+++ lib/characters/alignment.ex
@@ -1,22 +1,22 @@
 defmodule Alignment do
-  import Algae.Maybe
+  alias Algae.Maybe
 
   @type t :: Nothing.t | Good.t
                        | Evil.t
                        | Neutral.t
 
   defmodule Good do
-    @type t :: %Algae.Maybe.Just{just: %Good{}}
+    @type t :: %Maybe.Just{just: %Good{}}
     defstruct []
   end
 
   defmodule Evil do
-    @type t :: %Algae.Maybe.Just{just: %Evil{}}
+    @type t :: %Maybe.Just{just: %Evil{}}
     defstruct []
   end
 
   defmodule Neutral do
-    @type t :: %Algae.Maybe.Just{just: %Neutral{}}
+    @type t :: %Maybe.Just{just: %Neutral{}}
     defstruct []
   end
 
